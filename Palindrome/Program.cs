b7de76b644ed8bb4cab317cf788d6255a3a398ba
Nikita77227 <Nikita77227@DESKTOP-029TR2H>
2022-10-19 22:01:38 +0300

    Добавьте файлы проекта.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите слово");
+string w = Console.ReadLine();
+char[] word = w.ToCharArray();
+bool check = false;
+void Check()
+{
+    foreach (char check1 in word)
+    {
+        foreach (char check2 in word.Reverse())
+        {
+
+            if (check2 == check1)
+            {
+                check = true;
+            }
+            else if (check2 != check1)
+            {
+                check = false;
+            }
+        }
+    }
+    if (check == true)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Слово палиндром");
+        Console.WriteLine();
+    }
+    else if (check == false)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Слово не палиндром");
+        Console.WriteLine();
+    }
+}
+Check();
